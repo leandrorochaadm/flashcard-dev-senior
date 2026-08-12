3f5d53d9f1bc97adca5cd096459faee6d62acce9
LeandroRocha <leandrorochaadm@gmail.com>
2026-08-12 09:47:23 -0300

    chore: bump build number to 4

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -21,7 +21,7 @@ abstract final class AppVersion {
   /// `version:` of pubspec.yaml, after the `+`.
   static const build = String.fromEnvironment(
     'BUILD_NUMBER',
-    defaultValue: '3',
+    defaultValue: '4',
   );
 
   /// Short hash of the commit the bundle was built from. Empty when the app
